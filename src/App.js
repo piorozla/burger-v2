0df3cd5fe07ee7ccf9f1c1f828331f5b9c085e5a
Piorozla <piorozla@o2.pl>
2018-08-19 11:02:41 +0100

    added test text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h2>Olivia nie marudzi</h2>
         </p>
       </div>
     );
