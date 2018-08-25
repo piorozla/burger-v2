80b2b6b5fb76d3d3a2080eea8ca312ef5ad8913f
Piorozla <piorozla@o2.pl>
2018-08-25 13:03:07 +0100

    added message when ingredients are empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,11 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const Burger = props => {
   let transformedIngredients = Object.keys(props.ingredients).map((ingredient, ingIndex) => 
     [...Array(props.ingredients[ingredient])].map((val, valIndex) => <BurgerIngredient key={ingIndex + ':' + valIndex} type={ingredient} />)
-  );
+  ).reduce((arr, el)=> {return arr.concat(el)}, []);
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
