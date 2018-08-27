32df7dcbc837d5b52b7031c08f50b6f0af9040a3
Piorozla <piorozla@o2.pl>
2018-08-27 13:44:05 +0100

    added BuildControls component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,20 +1,22 @@
 import React, { Component, Fragment } from 'react';
+
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 1,
-      bacon: 1, 
+      bacon: 1,
       cheese: 3,
-      meat: 2
-    }
-  }
+      meat: 2,
+    },
+  };
   render() {
     return (
       <Fragment>
-        <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Fragment>
     );
   }
