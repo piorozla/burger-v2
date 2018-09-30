7219afe80fd69177541b1a91e62286654444c0c5
Piorozla <piorozla@o2.pl>
2018-09-30 11:05:28 +0100

    updated burgerBuilder component tests

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -3,7 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 
 import BurgerBuilder from './BurgerBuilder';
+import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 configure({ adapter: new Adapter() });
 
@@ -24,6 +27,41 @@ describe('<BurgerBuilder/>', () => {
     wrapper = shallow(<BurgerBuilder />);
     wrapper.setState(state);
   });
+  it('should render <Modal/> element', done => {
+    expect(wrapper.find(Modal)).toHaveLength(1);
+    expect(wrapper.find(Modal).props()['show']).toEqual(wrapper.state().purchasing);
+    expect(wrapper.find(Modal).props()['modalClosed']).toEqual(wrapper.instance().purchaseCancelHandler);
+    expect(wrapper.find(Modal).props()['children'].type).toEqual(OrderSummary);
+    expect(
+      wrapper
+        .find(Modal)
+        .children()
+        .at(0)
+        .props()['ingredients']
+    ).toEqual(wrapper.state().ingredients);
+    expect(
+      wrapper
+        .find(Modal)
+        .children()
+        .at(0)
+        .props()['purchaseCanceled']
+    ).toEqual(wrapper.instance().purchaseCancelHandler);
+    expect(
+      wrapper
+        .find(Modal)
+        .children()
+        .at(0)
+        .props()['purchaseContinued']
+    ).toEqual(wrapper.instance().purchaseContinueHandler);
+    expect(
+      wrapper
+        .find(Modal)
+        .children()
+        .at(0)
+        .props()['price']
+    ).toEqual(wrapper.state().totalPrice);
+    done();
+  });
   it('should render <BuildControls/> element', () => {
     expect(wrapper.find(BuildControls)).toHaveLength(1);
     expect(wrapper.find(BuildControls).props()['ingredientAdded']).toEqual(wrapper.instance().addIngredientHandler);
@@ -40,56 +78,63 @@ describe('<BurgerBuilder/>', () => {
     expect(wrapper.find(BuildControls).props()['purchasable']).toEqual(false);
     expect(wrapper.find(BuildControls).props()['ordered']).toEqual(wrapper.instance().purchaseHandler);
   });
-  describe('updatePurchaseState', () => {
-    it('should check if any ingredients are present', done => {
-      expect(wrapper.instance().updatePurchaseState(state.ingredients)).toEqual(false);
-      wrapper.setState({
-        ingredients: {
-          salad: 1,
-        },
+  it('should render <Burger/> element', done => {
+    expect(wrapper.find(Burger)).toHaveLength(1);
+    expect(wrapper.find(Burger).props()['ingredients']).toEqual(wrapper.state().ingredients);
+    done();
+  });
+  describe('methods', () => {
+    describe('updatePurchaseState', () => {
+      it('should check if any ingredients are present', done => {
+        expect(wrapper.instance().updatePurchaseState(state.ingredients)).toEqual(false);
+        wrapper.setState({
+          ingredients: {
+            salad: 1,
+          },
+        });
+        expect(wrapper.instance().updatePurchaseState(state.ingredients)).toEqual(false);
+        done();
       });
-      expect(wrapper.instance().updatePurchaseState(state.ingredients)).toEqual(false);
-      done();
     });
-  });
-  describe('addIngredientHandler', () => {
-    it('should check if any ingredients are present', done => {
-      wrapper.setState(state);
-      wrapper.instance().addIngredientHandler('salad');
-      expect(wrapper.state('ingredients').salad).toEqual(1);
-      expect(wrapper.state('totalPrice')).toEqual(4.5);
-      expect(wrapper.state('purchasable')).toEqual(true);
-      done();
-    });
-  });
-  describe('addIngredientHandler', () => {
-    it('should check if any ingredients are present', done => {
-      wrapper.setState({
-        ingredients: {
-          salad: 1,
-        },
+    describe('addIngredientHandler', () => {
+      it('should check if any ingredients are present', done => {
+        wrapper.setState(state);
+        wrapper.instance().addIngredientHandler('salad');
+        expect(wrapper.state('ingredients').salad).toEqual(1);
+        expect(wrapper.state('totalPrice')).toEqual(4.5);
+        expect(wrapper.state('purchasable')).toEqual(true);
+        done();
       });
-      wrapper.instance().removeIngredientHandler('salad');
-      expect(wrapper.state('ingredients').salad).toEqual(0);
-      expect(wrapper.state('totalPrice')).toEqual(4);
-      expect(wrapper.state('purchasable')).toEqual(false);
-      done();
     });
-  });
-  describe('purchaseHandler', () => {
-    it('should change purchasing to true', done => {
-      wrapper.setState({ purchasing: false });
-      wrapper.instance().purchaseHandler();
-      expect(wrapper.state('purchasing')).toEqual(true);
-      done();
+    describe('addIngredientHandler', () => {
+      it('should check if any ingredients are present', done => {
+        wrapper.setState({
+          ingredients: {
+            salad: 1,
+          },
+        });
+        wrapper.instance().removeIngredientHandler('salad');
+        expect(wrapper.state('ingredients').salad).toEqual(0);
+        expect(wrapper.state('totalPrice')).toEqual(4);
+        expect(wrapper.state('purchasable')).toEqual(false);
+        done();
+      });
     });
-  });
-  describe('purchaseCancelHandler', () => {
-    it('should change purchasing to false', done => {
-      wrapper.setState({ purchasing: true });
-      wrapper.instance().purchaseCancelHandler();
-      expect(wrapper.state('purchasing')).toEqual(false);
-      done();
+    describe('purchaseHandler', () => {
+      it('should change purchasing to true', done => {
+        wrapper.setState({ purchasing: false });
+        wrapper.instance().purchaseHandler();
+        expect(wrapper.state('purchasing')).toEqual(true);
+        done();
+      });
+    });
+    describe('purchaseCancelHandler', () => {
+      it('should change purchasing to false', done => {
+        wrapper.setState({ purchasing: true });
+        wrapper.instance().purchaseCancelHandler();
+        expect(wrapper.state('purchasing')).toEqual(false);
+        done();
+      });
     });
   });
 });
