4e599b83529834fb36887ec3738f0a52e912bf74
Piorozla <piorozla@o2.pl>
2018-10-22 19:08:42 +0100

    added removal of unneeded interceptors

--- src/hoc/WithErrorHandler/WithErrorHandler.js
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -7,17 +7,22 @@ const WithErrorHandler = (WrappedComponent, axios) => {
     state = {
       error: null,
     };
-    componentDidMount() {
-      axios.interceptors.request.use(req => {
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error });
         }
       );
+    }
+
+    comopenentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
