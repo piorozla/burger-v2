09a6c78ba1ad84584d0d14134183ac32db420765
Piorozla <piorozla@o2.pl>
2018-12-29 13:11:47 +0000

    updated index to use redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import reducer from './store/reducers/reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers());
 
 const app = (
   <Provider store={store}>
